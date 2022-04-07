7dcd87fad062329044556c19362764f60a1171f9
Bernardo Kraczkowski <bernardo.kraczkowski@nsh.com.br>
2022-04-07 11:15:36 -0300

    Initial design

---
+++ src/Tmdb.js
@@ -0,0 +1,105 @@
+const API_KEY = '940d83928ffef2d8e009bcfea7a7ca55'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+/*
+- Originais Netflix
+- Em destaque
+- Em alta 
+- Ação
+- Comedia
+- Terror 
+- Romance
+- Documentarios
+*/
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`)
+  const json = await req.json()
+  return json
+}
+
+export default {
+  getHomeList: async () => {
+    return [
+      {
+        slug: 'originals',
+        title: 'Originais do Netflix',
+        items: await basicFetch(
+          `/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'treding',
+        title: 'Recomendados para Você',
+        items: await basicFetch(
+          `/trending/all/week?language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'toprated',
+        title: 'Em alta',
+        items: await basicFetch(
+          `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'actions',
+        title: 'Ação',
+        items: await basicFetch(
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'comedy',
+        title: 'Comedia',
+        items: await basicFetch(
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        items: await basicFetch(
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        items: await basicFetch(
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentarios',
+        items: await basicFetch(
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`,
+        ),
+      },
+    ]
+  },
+
+  getMovieInfo: async (movieId, type) => {
+    let info = {}
+
+    if (movieId) {
+      switch (type) {
+        case 'movie':
+          info = await basicFetch(
+            `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`,
+          )
+          break
+        case 'tv':
+          info = await basicFetch(
+            `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`,
+          )
+          break
+        default:
+          info = null
+          break
+      }
+    }
+    return info;
+  },
+}
